d7d162c502234be943a2c8481ed09a9de279d9c2
assoares383 <alexandressoares383@gmail.com>
2021-04-13 20:39:02 -0300

    delete folder backend

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++
@@ -1,8 +0,0 @@
-import Appointment from '../infra/typeorm/entities/Appointment';
-import ICreateAppointmentDTO from './../dtos/ICreateAppointmentDTO';
-interface IAppointmentsRespository {
-  create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
-}
-
-export default IAppointmentsRespository;
