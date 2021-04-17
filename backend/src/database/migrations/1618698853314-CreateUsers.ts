a5198bccf738eb2f21956ebaec2f5352054aa628
assoares383 <alexandressoares383@gmail.com>
2021-04-17 20:09:37 -0300

    model and user migration

--- backend/src/database/migrations/1618698853314-CreateUsers.ts
+++ backend/src/database/migrations/1618698853314-CreateUsers.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export default class CreateAppointments1618602347266 implements MigrationInterface {
+export default class CreateUsers1618698853314 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.createTable(
         new Table({
-          name: 'appointments',
+          name: 'users',
           columns: [
             {
               name: 'id',
@@ -15,13 +15,19 @@ export default class CreateAppointments1618602347266 implements MigrationInterfa
               default: 'uuid_generate_v4()'
             },
             {
-              name: 'provider',
+              name: 'name',
               type: 'varchar',
               isNullable: false
             },
             {
-              name: 'date',
-              type: 'timestamp with time zone',
+              name: 'email',
+              type: 'varchar',
+              isUnique: true,
+              isNullable: false
+            },
+            {
+              name: 'password',
+              type: 'varchar',
               isNullable: false
             },
             {
@@ -40,7 +46,7 @@ export default class CreateAppointments1618602347266 implements MigrationInterfa
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.dropTable('appointments');
+      await queryRunner.dropTable('users');
     }
 
 }
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -5,16 +5,20 @@ import {
   CreateDateColumn, 
   UpdateDateColumn 
 } from 'typeorm';
-@Entity('appointments')
+
+@Entity('users')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider_id: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
