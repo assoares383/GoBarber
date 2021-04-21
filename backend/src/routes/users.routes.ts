1f6d87532ac23dfd40d8637ffd34e6b781d79f4a
assoares383 <alexandressoares383@gmail.com>
2021-04-20 21:18:38 -0300

    authentication and image upload

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -13,7 +19,7 @@ usersRouter.post('/', async (request, response) => {
     const user = await createUser.execute({
       name,
       email,
-      password
+      password,
     });
 
     const userWithoutPassword = {
@@ -25,9 +31,37 @@ usersRouter.post('/', async (request, response) => {
     };
 
     return response.json(userWithoutPassword);
-  } catch(err) {
+  } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json(userWithoutPassword);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,17 +2,19 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World 2021' });
-})
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started!');
