f89e497df0024498a5caf9f0fe2d92a2cd95e496
assoares383 <alexandressoares383@gmail.com>
2021-04-28 18:19:38 -0300

    testing user creation

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
@@ -17,7 +19,24 @@ describe('CretaeAppointment', () => {
     expect(appointment.provider_id).toBe('123123');
   });
 
-  it('should not be able to create two appointments on the same time', () => {
-    
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+
+    const appointmentDate = new Date(2021, 4, 25, 11);
+
+    await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: new Date(),
+        provider_id: '123123',
+      })).rejects.toBeInstanceOf(AppError);
   });
 });
