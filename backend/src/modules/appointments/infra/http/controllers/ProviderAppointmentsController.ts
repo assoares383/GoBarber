cf08cbb542e809e03a0a10aebb61eee834137707
assoares383 <alexandressoares383@gmail.com>
2021-05-07 18:22:00 -0300

    provider's agenda

---
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,23 @@
+import { container } from 'tsyringe';
+import { Request, Response } from "express";
+
+import ListProviderAppointments from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): 
+    Promise<Response> {
+      const provider_id = request.user.id;
+      const { day, month , year } = request.body;
+
+      const listProviderAppointments = container.resolve(ListProviderAppointments);
+
+      const appointments = await listProviderAppointments.execute({
+        provider_id,
+        day, 
+        month, 
+        year
+      });
+
+      return response.json(appointments);
+    }
+} 
