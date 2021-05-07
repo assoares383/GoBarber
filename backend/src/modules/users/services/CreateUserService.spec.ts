5031eda94332dd41724ab217eff7d28b9b41fa0a
assoares383 <alexandressoares383@gmail.com>
2021-05-07 09:47:48 -0300

    scheduling rules

--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -22,24 +22,24 @@ describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const user = await createUser.execute({
       name: 'John Doe',
-      email: 'johndoe@example.com.br',
+      email: 'johndoe@example.com',
       password: '123123',
     });
 
     expect(user).toHaveProperty('id');
   });
 
-  it('should not be able to create a new user with same email another', async () => {
+  it('should not be able to create a new user with email from another', async () => {
     await createUser.execute({
       name: 'John Doe',
-      email: 'johndoe@example.com.br',
+      email: 'johndoe@example.com',
       password: '123123',
     });
 
     await expect(
       createUser.execute({
         name: 'John Doe',
-        email: 'johndoe@example.com.br',
+        email: 'johndoe@example.com',
         password: '123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
