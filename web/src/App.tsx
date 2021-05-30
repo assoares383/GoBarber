b26a3b9805f1370e307d4625bd76a747549badaa
assoares383 <alexandressoares383@gmail.com>
2021-05-30 20:09:02 -0300

    refactor

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-import GlobalStyle from './styles/global';
-
-import AppProvider from './hooks';
-
-import Routes from './routes';
-
-const App: React.FC = () => (
-  <Router>
-    <AppProvider>
-      <Routes />
-    </AppProvider>
-
-    <GlobalStyle />
-  </Router>
-);
+const App: React.FC = () => <h1>Hello World! </h1>
 
 export default App;
