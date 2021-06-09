5093b395438d2ae25e4b5e32b7f8904ee519bd8f
assoares383 <alexandressoares383@gmail.com>
2021-06-09 19:10:17 -0300

    creating header

---
+++ web/src/pages/Dashboard/styles.ts
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.header`
+  padding: 32px 0;
+  background-color: #28262e;
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+
+  > img {
+    height: 80px;
+  }
+
+  button {
+    margin-left: auto;
+    background-color: transparent;
+    border: 0;
+
+    svg {
+      color: #999591;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 80px;
+
+  img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span {
+      color: #f4ede8;
+    }
+
+    a {
+      text-decoration: none;
+      color: #ff9000;
+
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+  }
+`;
+
+export const Content = styled.main`
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    margin-top: 8px;
+    color: #ff9000;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    span + span::before {
+      content: '';
+      width: 1px;
+      height: 12px;
+      background-color: #ff9000;
+      margin: 0 8px;
+    }
+  }
+`;
