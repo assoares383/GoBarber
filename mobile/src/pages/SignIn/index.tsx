c231a8b817ad7579d7f8e40675fb6b3088d19b00
assoares383 <alexandressoares383@gmail.com>
2021-07-05 19:38:09 -0300

    chore: add input and button

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, ForgotPassword, ForgotPasswordText } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -10,6 +13,31 @@ const SignIn: React.FC = () => (
         <Image source={logoImg} />
 
         <Title>Faça seu logon</Title>
+
+        <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            name="email"
+            icon="mail"
+            placeholder="E-mail"
+            returnKeyType="next"
+        />
+        <Input
+            name="password"
+            icon="lock"
+            placeholder="Senha"
+            secureTextEntry
+            returnKeyType="send"
+        />
+
+        <Button onPress={() => {
+            console.log('clicou')
+        }}>Entrar</Button>
+
+        <ForgotPassword>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
     </Container>
 );
 
