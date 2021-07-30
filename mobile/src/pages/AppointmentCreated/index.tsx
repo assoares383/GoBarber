9e2e9684b917919af64837c26d30e12701bda528
assoares383 <alexandressoares383@gmail.com>
2021-07-30 18:12:42 -0300

    feat: creating screen structure

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { Container, Description, OkButton, OkButtonText, Title } from './styles';
 
 const AppointmentCreated: React.FC = () => {
-    return <View />;
+    const { reset } = useNavigation();
+
+    const handleOkPressed = useCallback(() => {
+        reset({
+            routes: [{ name: 'Dashboard' }],
+            index: 0
+        });
+    }, []);
+
+    return (
+        <Container>
+            <Icon name="check" size={80} color="#04d361" />
+
+            <Title>Agendamento concluído</Title>
+            <Description>Terça, dia 03 de agosto de 2021 às 16:00h</Description>
+
+            <OkButton onPress={handleOkPressed}>
+                <OkButtonText>Ok</OkButtonText>
+            </OkButton>
+        </Container>
+    );
 };
 
 export default AppointmentCreated;
